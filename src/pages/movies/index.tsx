785115fc95a4dbefe7df363563eccd254c3d58e7
Andrew Brunker <andrew@brunker.net.au>
2024-04-02 23:17:30 +1100

    feat: add retrival of movies and shows for the directory pages

--- src/pages/movies/index.tsx
+++ src/pages/movies/index.tsx
@@ -1,6 +1,50 @@
-import React from "react"
+import Plex from "@/services/plex.ts"
+import { plex_libs, plex_movies } from "@/services/plex.interfaces.ts"
+import { useEffect, useState } from "react"
 
 const Movies = () => {
+    const [lib_response, set_lib_response] = useState<undefined | plex_libs>(undefined)
+    const [movies_id, set_movies_id] = useState<undefined | number>(undefined)
+    const [movies, set_movies] = useState<undefined | plex_movies>(undefined)
+
+    const lib_get = async () => {
+        if (lib_response) {
+            return lib_response
+        }
+
+        const response = await Plex.libraries_get()
+        const data = await response.json() as plex_libs
+        set_lib_response(data)
+        return lib_response
+    }
+
+    useEffect(() => {
+        lib_get()
+            .then(() => {
+                if (
+                    undefined !== lib_response &&
+                    undefined === movies_id
+                ) {
+                    lib_response.MediaContainer.Directory.forEach(itm => {
+                        if ("movie" === itm.type) {
+                            set_movies_id(parseInt(itm.key))
+                        }
+                    })
+                }
+            })
+            .then(() => {
+                if (undefined !== movies_id) {
+                    Plex.movies_get(movies_id)
+                        .then(response => response.json())
+                        .then((result: plex_movies) => {
+                            set_movies(result)
+                        })
+                        .catch(error => console.error(error))
+                }
+            })
+            .catch(error => console.error(error))
+    })
+
     return (
         <h1>Movies</h1>
     )
--- src/pages/shows/index.tsx
+++ src/pages/shows/index.tsx
@@ -1,6 +1,50 @@
-import React from "react"
+import Plex from "@/services/plex.ts"
+import { plex_libs, plex_shows } from "@/services/plex.interfaces.ts"
+import { useEffect, useState } from "react"
 
 const Shows = () => {
+    const [lib_response, set_lib_response] = useState<undefined | plex_libs>(undefined)
+    const [shows_id, set_shows_id] = useState<undefined | number>(undefined)
+    const [shows, set_shows] = useState<undefined | plex_shows>(undefined)
+
+    const lib_get = async () => {
+        if (lib_response) {
+            return lib_response
+        }
+
+        const response = await Plex.libraries_get()
+        const data = await response.json() as plex_libs
+        set_lib_response(data)
+        return lib_response
+    }
+
+    useEffect(() => {
+        lib_get()
+            .then(() => {
+                if (
+                    undefined !== lib_response &&
+                    undefined === shows_id
+                ) {
+                    lib_response.MediaContainer.Directory.forEach(itm => {
+                        if ("show" === itm.type) {
+                            set_shows_id(parseInt(itm.key))
+                        }
+                    })
+                }
+            })
+            .then(() => {
+                if (undefined !== shows_id) {
+                    Plex.shows_get(shows_id)
+                        .then(response => response.json())
+                        .then((result: plex_shows) => {
+                            set_shows(result)
+                        })
+                        .catch(error => console.error(error))
+                }
+            })
+            .catch(error => console.error(error))
+    })
+
     return (
         <h1>Shows</h1>
     )

dd28dfd6acfe42653ae3b880cbe5a4deb3401d24
Andrew Brunker <andrew@brunker.net.au>
2024-04-13 23:14:43 +1000

    feat: implement basic nav bar

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,11 +1,44 @@
+import { Link } from "@tanstack/react-router"
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "@/components/ui/navigation-menu"
 import { Outlet, createRootRoute } from "@tanstack/react-router"
 import { TanStackRouterDevtools } from "@tanstack/router-devtools"
+import { memo } from "react"
 
-export const Route = createRootRoute({
-    component: () => (
+const Root = () => {
+    return (
         <>
+            <NavigationMenu>
+                <NavigationMenuList>
+                    <NavigationMenuItem>
+                        <Link to="/">
+                            <NavigationMenuLink>
+                                Home
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link to="/movies">
+                            <NavigationMenuLink>
+                                Movies
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                    <NavigationMenuItem>
+                        <Link to="/shows">
+                            <NavigationMenuLink>
+                                Shows
+                            </NavigationMenuLink>
+                        </Link>
+                    </NavigationMenuItem>
+                </NavigationMenuList>
+            </NavigationMenu>
             <Outlet />
             <TanStackRouterDevtools />
         </>
     )
+
+}
+
+export const Route = createRootRoute({
+    component: memo(Root)
 })

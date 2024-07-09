05c45c56c6000bc8dca61075b1207e58b50e1f07
Andrew Brunker <andrew@brunker.net.au>
2024-07-09 20:40:31 +1000

    fix: resolve compile issue with react compiler for babel

--- vite.config.ts
+++ vite.config.ts
@@ -3,17 +3,10 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
 
-const ReactCompilerConfig = {}
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
-		react({
-			babel: {
-				plugins: [
-					["babel-plugin-react-compiler", ReactCompilerConfig]
-				]
-			}
-		}),
+		react(),
 		TanStackRouterVite(),
 	],
 	resolve: {

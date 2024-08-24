13e5ea5ad94071f49d94cd1c9cc78e5008978e52
Andrew Brunker <andrew@brunker.net.au>
2024-08-24 10:22:17 +1000

    refactor: movie the card information into the counter

--- src/components/plex/libraryCounter.tsx
+++ src/components/plex/libraryCounter.tsx
@@ -1,5 +1,8 @@
-import { LibraryType } from "@/lib/enums/plexCommon"
-import { MoviesStruct } from "@/slices/movies"
+import { useEffect, useState } from "react"
+import { useAppDispatch, useAppSelector } from "@/hooks.ts"
+import { useGetMovieLibraryQuery } from "@/services/plex.ts"
+import { LibraryType } from "@/lib/enums/plexCommon.ts"
+import { MoviesStruct, createUpdateMovieCollectionFull } from "@/slices/movies.ts"
 
 interface LibraryCounterProps {
 	libraryId: string,
@@ -13,9 +16,52 @@ export function LibraryCounter(
 		details = false
 	}: LibraryCounterProps
 ) {
-	const movies: MoviesStruct[] | null = null
-	const shows: null = null
-	const audio: null = null
+	const [ content, setContent ] = useState(<></>)
+	const collection: MoviesStruct[] | null = null
+
+	let data: any = undefined
+	let error: boolean = false
+	let isLoading: boolean = false
+
+	if (type === LibraryType.Movie) {
+		collection = useAppSelector(state => state.movies.movies))
+		{ data, error, isLoading } = useGetMovieLibraryQuery(
+			parseInt(libraryId)
+		)
+	}
+
+	if (!error && data !== undefined) {
+		dispatch(createUpdateMovieCollectionFull({
+			library: libraryId,
+			movies: data.mediaContainer.metadata
+		}))
+	}
+
+	useEffect(() => {
+		let libraryCount = -1
+
+		if (type === LibraryType.Movie) {
+			libraryCount = (collection as MoviesStruct[])
+				.filter(movie => movie.library === libraryId).length
+		}
+
+		if (isLoading) {
+			setContent(<div>Loading ...</div>)
+		} else if (colleaction === undefined || libraryCount < 1) {
+			if (details) {
+				// TODO: Implement nice formatted message; ie. No Movies Found
+			} else {
+				setContent(<>0</>)
+			}
+		} else {
+			if (details) {
+				// TODO: Implement nice formatted mmessage;  ie. 99 Movies Found
+			} else {
+				setContent(<>{libraryCount}</>)
+			}
+		}
+	}, [type, details, isLoading, setContent, libraryId, collection])
+
 
 	return <>
 		<h4>To Be Implemented</h4>
